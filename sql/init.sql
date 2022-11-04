d7e20105568d0a93c9a333d3c349b2861465f016
Wonsang <temporaryaion1@hotmail.com>
2022-11-04 23:53:52 +0900

    verbosely add primary key to posts and comments

--- sql/init.sql
+++ sql/init.sql
@@ -1,11 +1,14 @@
 DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS posts CASCADE;
 DROP TABLE IF EXISTS comments CASCADE;
-DROP TABLE IF EXISTS likes CASCADE;
 
 DROP SEQUENCE IF EXISTS users_id_seq CASCADE;
+DROP SEQUENCE IF EXISTS posts_id_seq CASCADE;
+DROP SEQUENCE IF EXISTS comments_id_seq CASCADE;
 
 CREATE SEQUENCE users_id_seq;
+CREATE SEQUENCE posts_id_seq;
+CREATE SEQUENCE comments_id_seq;
 
 CREATE TABLE users (
     id INT NOT NULL DEFAULT nextval('users_id_seq'),
@@ -17,23 +20,31 @@ CREATE TABLE users (
 );
 
 ALTER SEQUENCE users_id_seq
-OWNED BY users.id;
+    OWNED BY users.id;
 
 CREATE TABLE posts (
-    id serial PRIMARY KEY,
+    id INT NOT NULL DEFAULT nextval('posts_id_seq'),
     author_id INT NOT NULL,
     create_date TIMESTAMPTZ NOT NULL,
     update_date TIMESTAMPTZ,
     content TEXT,
     title VARCHAR(255),
+    CONSTRAINT "posts_pkey" PRIMARY KEY ("id"),
     FOREIGN KEY (author_id) REFERENCES users (id)
 );
 
+ALTER SEQUENCE posts_id_seq
+    OWNED BY posts.id;
+
 CREATE TABLE comments (
-    id serial PRIMARY KEY,
+    id INT NOT NULL DEFAULT nextval('comments_id_seq'),
     content TEXT,
     user_id INT,
     post_id INT,
+    CONSTRAINT "comments_pkey" PRIMARY KEY ("id"),
     FOREIGN KEY (user_id) REFERENCES users (id),
     FOREIGN KEY (post_id) REFERENCES posts (id)
 );
+
+ALTER SEQUENCE comments_id_seq
+    OWNED by comments.id;
