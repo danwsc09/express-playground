17284e79caf06842b3663f87491b042dcc7601e2
Wonsang <temporaryaion1@hotmail.com>
2022-11-13 22:44:35 +0900

    add sample data in sql init script

--- sql/init.sql
+++ sql/init.sql
@@ -50,3 +50,18 @@ CREATE TABLE comments (
 
 ALTER SEQUENCE comments_id_seq
     OWNED by comments.id;
+
+INSERT INTO users(email, password, created_on) 
+values('pew@pew.com', '$2b$10$m9EbuRJ1imAa/NQ1IIXs3./cHBnNx.WVe1DWjrfN1wwl10EHTJHd.', '2022-11-04T14:36:09.828Z');
+
+INSERT INTO users(email, password, created_on) 
+VALUES('secret@gmail.com', '$2a$10$1/sSxsV/ImrYqMSNQ3xX7./v1gJqaWUJ/dQEGTrv3qLUxve.farFW', '2022-11-13T14:36:09.828Z');
+
+INSERT INTO posts(author_id, create_date, content, title)
+VALUES(1, '2022-11-11T14:26:09.828Z', 'rust is the most blazingly fastest in the planeto', 'rust of the day');
+
+INSERT INTO posts(author_id, create_date, content, title)
+VALUES(1, '2022-11-11T18:22:09.828Z', 'iphone flip wins','iphone vs samsung');
+
+INSERT INTO comments(user_id, post_id, content, create_date)
+VALUES(2, 1, 'i agree rust is faster than a cheetah on steroids', '2022-11-12T09:22:09.828Z');
