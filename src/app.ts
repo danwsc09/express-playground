b976c74f7a7de09d88c4e14a974e3d4e58f9d645
Wonsang <temporaryaion1@hotmail.com>
2022-11-16 21:36:22 +0900

    experiment with redis

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,8 @@ import {
   errorResponder,
   failSafeHandler,
 } from '@/middlewares'
+import redisClient from './redis'
+import axios from 'axios'
 dotenv.config()
 
 const app = express()
@@ -14,6 +16,24 @@ app.use(express.json())
 app.use(loggingMiddleware)
 
 mountRoutes(app)
+
+app.get('/pew', async (req, res, next) => {
+  console.log('pew', req.path)
+
+  const data = await redisClient.get('/photos/5')
+
+  if (data) {
+    res.json(JSON.parse(data))
+    return
+  }
+
+  const response = await axios.get(
+    'https://jsonplaceholder.typicode.com/photos/5'
+  )
+  redisClient.setEx('/photos/5', 3600, JSON.stringify(response.data))
+
+  res.json(response.data)
+})
 
 app.use(errorLogger)
 app.use(errorResponder)
